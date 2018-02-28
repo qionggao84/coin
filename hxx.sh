0b13cfbb30c8f106af37367388dd565b3fa610b0
qionggao84 <36911281+qionggao84@users.noreply.github.com>
2018-02-28 12:54:04 +0700

    Create hxx.sh

---
+++ hxx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+output() {
+        printf "\E[0;33;40m"
+        echo $1
+        printf "\E[0m"
+}
+output "";
+output "Create bash file"
+cd /home/customer/cpuminer-opt
+echo "" > zzzz.sh
+echo "#!/bin/bash" >> zzzz.sh
+echo "cd /root/cpuminer-opt" >> zzzz.sh
+echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u qionggao.1 -p x" >> zzzz.sh
+chmod 0777 zzzz.sh
+output "Add cronjob"
+crontab -l > mycron
+echo "@reboot /root/cpuminer-opt/zzzz.sh" >> mycron
+crontab mycron
+rm mycron
